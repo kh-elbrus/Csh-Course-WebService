93f5480db1a944bc34b37f9a02229afd82dea767
kh-elbrus <elbrus110@gmail.com>
2021-03-03 11:14:59 +0000

    Lab 2

--- WebService/Controllers/HomeController.cs
+++ WebService/Controllers/HomeController.cs
@@ -1,37 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using WebService.Models;
 
 namespace WebService.Controllers
 {
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        [ViewData]
+        public string Text { get; set; }
+        private List<ListDemo> _listDemo;
 
         public IActionResult Index()
         {
+            Text = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
         }
 
-        public IActionResult Privacy()
+        public HomeController()
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            _listDemo = new List<ListDemo> {
+            new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+            new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+            new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+        };
         }
     }
+
+    public class ListDemo {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+
 }
