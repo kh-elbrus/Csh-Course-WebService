414bcc53506fab9ada888a2da1432c4353008036
kh-elbrus <elbrus110@gmail.com>
2021-03-03 12:12:17 +0000

    Lab 3

---
+++ WebService/Components/MenuViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Models;
+
+namespace WebService.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private List<MenuItem> _menuItems = new List<MenuItem> {
+            new MenuItem{ Controller="Home", Action="Index", Text="Lab 2" },
+            new MenuItem{ Controller="Product", Action="Index", Text="Католог" },
+            new MenuItem{ IsPage=true, Area="Admin", Page="/Index", Text="Администрирование" }
+        };
+
+        public IViewComponentResult Invoke()
+        {
+            var controller = ViewContext.RouteData.Values["controller"];
+            var page = ViewContext.RouteData.Values["page"];
+            var area = ViewContext.RouteData.Values["area"];
+
+            foreach (var item in _menuItems) {
+                var _matchController = controller?.Equals(item.Controller) ?? false;
+                var _matchArea = area?.Equals(item.Area) ?? false;
+                if (_matchController || _matchArea)
+                {
+                    item.Active = "active";
+                }
+            }
+            return View(_menuItems);
+        }
+    };
+}
