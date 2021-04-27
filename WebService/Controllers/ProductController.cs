44bd359cc91e19a5f662b33f021d06f98c2953ea
Elbrus <elbrus.infosec@gmail.com>
2021-04-27 14:36:05 -0700

    Added Lab 7!

--- WebService/Controllers/ProductController.cs
+++ WebService/Controllers/ProductController.cs
@@ -4,16 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebLabsV05.DAL.Entities;
+using WebService.Extensions;
 using WebService.Models;
 
 namespace WebService.Controllers
 {
     public class ProductController : Controller
-
     {
         public List<Dish> _dishes;
         List<DishGroup> _dishGroups;
-
         int _pageSize;
 
         public ProductController()
@@ -22,50 +21,57 @@ namespace WebService.Controllers
             SetupData();
         }
 
-        public IActionResult Index(int? group, int pageNo=1)
+        [Route("Catalog")]
+        [Route("Catalog/Page_{pageNo}")]
+        public IActionResult Index(int? group, int pageNo = 1)
         {
-            var dishesFiltered = _dishes.Where(d => !group.HasValue || d.DishGroupId == group.Value);
+            var dishesFiltered = _dishes
+            .Where(d => !group.HasValue || d.DishGroupId == group.Value);
+            // Получить id текущей группы и поместить в TempData
             ViewData["Groups"] = _dishGroups;
             ViewData["CurrentGroup"] = group ?? 0;
-            var items = _dishes
-                .Skip((pageNo - 1) * _pageSize)
-                .Take(_pageSize)
-                .ToList();
-            return View(ListViewModel<Dish>.GetModel(dishesFiltered, pageNo, _pageSize));
+
+            var model = ListViewModel<Dish>.GetModel(dishesFiltered, pageNo, _pageSize);
+            if (Request.IsAjaxRequest())
+                return PartialView("_listpartial", model);
+            else
+                return View(model);
         }
-
-
+        /// <summary>
+        /// Инициализация списков
+        /// </summary>
         private void SetupData()
         {
             _dishGroups = new List<DishGroup>
-            {
-                new DishGroup {DishGroupId=1, GroupName="Стартеры"},
-                new DishGroup {DishGroupId=2, GroupName="Салаты"},
-                new DishGroup {DishGroupId=3, GroupName="Супы"},
-                new DishGroup {DishGroupId=4, GroupName="Основные блюда"},
-                new DishGroup {DishGroupId=5, GroupName="Напитки"},
-                new DishGroup {DishGroupId=6, GroupName="Десерты"}
-            };
+                {
+                    new DishGroup {DishGroupId=1, GroupName="Стартеры"},
+                    new DishGroup {DishGroupId=2, GroupName="Салаты"},
+                    new DishGroup {DishGroupId=3, GroupName="Супы"},
+                    new DishGroup {DishGroupId=4, GroupName="Основные блюда"},
+                    new DishGroup {DishGroupId=5, GroupName="Напитки"},
+                    new DishGroup {DishGroupId=6, GroupName="Десерты"}
+                };
             _dishes = new List<Dish>
-            {
-                new Dish {DishId = 1, DishName="Суп-харчо",
-                Description="Очень острый, невкусный",
-                Calories =200, DishGroupId=3, Image="Суп.jpg" },
-                new Dish { DishId = 2, DishName="Борщ",
-                Description="Много сала, без сметаны",
-                Calories =330, DishGroupId=3, Image="Борщ.jpg" },
-                new Dish { DishId = 3, DishName="Котлета пожарская",
-                Description="Хлеб - 80%, Морковь - 20%",
-
-                Calories =635, DishGroupId=4, Image="Котлета.jpg" },
-                new Dish { DishId = 4, DishName="Макароны по-флотски",
-                Description="С охотничьей колбаской",
-                Calories =524, DishGroupId=4, Image="Макароны.jpg" },
-                new Dish { DishId = 5, DishName="Компот",
-                Description="Быстро растворимый, 2 литра",
-                Calories =180, DishGroupId=5, Image="Компот.jpg" }
-            };
+                {
+                    new Dish {DishId = 1, DishName="Грибной суп",
+                    Description="Лук, картофель, сливки, грибы шампиньоны.",
+                    Calories =180, DishGroupId=3, Image="1.jpg" },
+                    new Dish { DishId = 2, DishName="Томатный суп",
+                    Description="Лук, чеснок, помидоры, итальянские травы, перец",
+                    Calories =330, DishGroupId=3, Image="2.jpg" },
+                    new Dish { DishId = 3, DishName="Салат сыттов",
+                    Description="Фасоль, корейская морковь, сыр, сухари, зелень.",
+                    Calories =120, DishGroupId=4, Image="3.jpg" },
+                    new Dish { DishId = 4, DishName="Салат с тунцом",
+                    Description="Тунец, салат, помидор. Подается в пите из пшеничной муки",
+                    Calories =240, DishGroupId=4, Image="2.jpg" },
+                    new Dish { DishId = 5, DishName="Морс Gedonia клюква",
+                    Description="500 мл, Вода очищенная, клюква, сахар, брусника.",
+                    Calories =55, DishGroupId=5, Image="3.jpg" },
+                    new Dish { DishId = 5, DishName="Fanta",
+                    Description="500 мл, Газированный напиток со вкусом апельсина.",
+                    Calories =40, DishGroupId=5, Image="1.jpg" }
+                };
         }
-
     }
 }
---
+++ WebService/Extensions/RequestExtrensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebService.Extensions
+{
+    public static class RequestExtrensions
+    {
+        public static bool IsAjaxRequest(this HttpRequest request)
+        {
+            return request
+
+            .Headers["x-requested-with"]
+            .Equals("XMLHttpRequest");
+        }
+    }
+}
